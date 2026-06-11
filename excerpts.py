2b7ff7f8332466717438d40187e4dc9a65d3a114
fsmeraldi <f.smeraldi@qmul.ac.uk>
2026-06-11 12:15:29 +0100

    Clean commit

---
+++ excerpts.py
@@ -0,0 +1,114 @@
+# from Herman Melville, Moby Dick; or, The Whale, ch. 1 (1851)
+moby_dick= """Call me Ishmael. Some years ago - never mind how long 
+precisely - having little or no money in my purse, and nothing particular to 
+interest me on shore, I thought I would sail about a little and see the 
+watery part of the world. It is a way I have of driving off the spleen and 
+regulating the circulation. Whenever I find myself growing grim about the 
+mouth; whenever it is a damp, drizzly November in my soul; whenever I find
+myself involuntarily pausing before coffin warehouses, and bringing up the
+rear of every funeral I meet; and especially whenever my hypos get such an
+upper hand of me, that it requires a strong moral principle to prevent me
+from deliberately stepping into the street, and methodically knocking
+people's hats off—then, I account it high time tozz get to sea as soon
+as I can. This is my substitute for pistol and ball. With a philosophical
+flourish Cato throws himself upon his sword; I quietly take to the ship.
+There is nothing surprising in this. If they but knew it, almost all men in
+their degree, some time or other, cherish very nearly the same feelings
+towards the ocean with me.
+
+There now is your insular city of the Manhattoes, belted round by wharves
+as Indian isles by coral reefs—commerce surrounds it with her surf. Right
+and left, the streets take you waterward. Its extreme downtown is the 
+battery, where that noble mole is washed by waves, and cooled by breezes,
+which a few hours previous were out of sight of land. Look at the crowds of 
+water-gazers there.
+
+Circumambulate the city of a dreamy Sabbath afternoon. Go from Corlears Hook 
+to Coenties Slip, and from thence, by Whitehall, northward. What do you see?
+- Posted like silent sentinels all around the town, stand thousands upon 
+thousands of mortal men fixed in ocean reveries. Some leaning against the 
+spiles; some seated upon the pier-heads; some looking over the bulwarks of 
+ships from China; some high aloft in the rigging, as if striving to get a 
+still better seaward peep. But these are all landsmen; of week days pent 
+up in lath and plaster - tied to counters, nailed to benches, clinched to 
+desks. How then is this? Are the green fields gone? What do they here?
+
+But look! here come more crowds, pacing straight for the water, and 
+seemingly bound for a dive. Strange! Nothing will content them but the 
+extremest limit of the land; loitering under the shady lee of yonder 
+warehouses will not suffice. No. They must get just as nigh the water as 
+they possibly can without falling in. And there they stand—miles of them -
+leagues. Inlanders all, they come from lanes and alleys, streets and avenues,
+- north, east, south, and west. Yet here they all unite. Tell me, does the 
+magnetic virtue of the needles of the compasses of all those ships attract 
+them thither?
+
+Once more. Say you are in the country; in some high land of lakes. Take
+almost any path you please, and ten to one it carries you down in a dale,
+and leaves you there by a pool in the stream. There is magic in it. Let
+the most absent-minded of men be plunged in his deepest reveries—stand that
+man on his legs, set his feet a-going, and he will infallibly lead you to
+water, if water there be in all that region. Should you ever be athirst in
+the great American desert, try this experiment, if your caravan happen to
+be supplied with a metaphysical professor. Yes, as every one knows,
+meditation and water are wedded for ever."""
+
+# From Carlo Collodi, Le avventure di Pinocchio. Storia di un burattino,
+# ch. 5 (1883)
+pinocchio= """Intanto comincio' a farsi notte, e Pinocchio, ricordandosi
+che  non aveva mangiato nulla, senti' un'uggiolina allo stomaco, che 
+somigliava  moltissimo all'appetito.
+
+Ma l'appetito nei ragazzi cammina presto, e difatti, dopo pochi minuti 
+l'appetito divento' fame, e la fame, dal vedere al non vedere, si converti' 
+in una fame da lupi, una fame da tagliarsi col coltello.
+
+Il povero Pinocchio corse subito al focolare, dove c'era una pentola che 
+bolliva e fece l'atto di scoperchiarla, per vedere che cosa ci fosse dentro, 
+ma la pentola era dipinta sul muro. Immaginatevi come resto'. Il suo naso, 
+che era gia' lungo, gli divento' piu' lungo almeno quattro dita.
+
+Allora si dette a correre per la stanza e a frugare per tutte le cassette 
+e per tutti i ripostigli in cerca di un po' di pane, magari un po' di pan 
+secco, un crosterello, un osso avanzato al cane, un po' di polenta muffita, 
+una lisca di pesce, un nocciolo di ciliegia, insomma di qualche cosa da 
+masticare: ma non trovo' nulla, il gran nulla, proprio nulla.
+
+E intanto la fame cresceva, e cresceva sempre e il povero Pinocchio non
+aveva altro sollievo che quello di sbadigliare: e faceva degli sbadigli
+cosi' lunghi, che qualche volta la bocca gli arrivava fino agli orecchi.
+E dopo avere sbadigliato, sputava, e sentiva che lo stomaco gli andava via.
+
+Allora piangendo e disperandosi, diceva:
+
+- Il Grillo-parlante aveva ragione. Ho fatto male a rivoltarmi al mio babbo
+e a fuggire di casa.... Se il mio babbo fosse qui, ora non mi troverei a
+morire di sbadigli! Oh! che brutta malattia che e' la fame! -
+
+Quand'ecco gli parve di vedere nel monte della spazzatura qualche cosa di
+tondo e di bianco, che somigliava tutto a un uovo di gallina. Spiccare un
+salto e gettarvisi sopra, fu un punto solo. Era un uovo davvero.
+
+La gioia del burattino e' impossibile descriverla: bisogna sapersela
+figurare. Credendo quasi che fosse un sogno, si rigirava quest'uovo fra
+le mani, e lo toccava e lo baciava, e baciandolo diceva:
+
+- E ora come dovro' cuocerlo? Ne faro' una frittata?... No, e' meglio
+cuocerlo nel piatto!... O non sarebbe piu' saporito se lo friggessi in
+padella? O se invece lo cuocessi a uso uovo a bere? No, la piu' lesta di
+tutte e' di cuocerlo nel piatto o nel tegamino: ho troppa voglia di
+mangiarmelo! -
+
+Detto fatto, pose un tegamino sopra un caldano pieno di brace accesa:
+messe nel tegamino, invece d'olio o di burro, un po' d'acqua: e quando
+l'acqua principio' a fumare, tac!... spezzo' il guscio dell'uovo, e fece
+l'atto di scodellarvelo dentro.
+
+Ma invece della chiara e del torlo scappo' fuori un pulcino tutto allegro
+e complimentoso, il quale facendo una bella riverenza disse:
+
+- Mille grazie, signor Pinocchio, d'avermi risparmiata la fatica di rompere
+il guscio! Arrivedella, stia bene e tanti saluti a casa! -
+
+Cio' detto, distese le ali, e, infilata la finestra che era aperta, se ne
+volo' via a perdita d'occhio."""
